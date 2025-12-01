2bf266f7896d41f209d4b91433f3c241648bb048
mxm <contact@moneyexmachina.com>
2025-12-01 13:49:09 +0000

    API: define public package API and __all__

--- src/mxm/pipeline/__init__.py
+++ src/mxm/pipeline/__init__.py
@@ -1,3 +1,14 @@
+"""
+Public API for mxm-pipeline.
+"""
+
+from .api import compile_flow, execute_flow
 from .spec import AssetDecl, FlowSpec, TaskSpec
 
-__all__ = ["AssetDecl", "FlowSpec", "TaskSpec"]
+__all__ = [
+    "AssetDecl",
+    "FlowSpec",
+    "TaskSpec",
+    "compile_flow",
+    "execute_flow",
+]
